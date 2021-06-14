d38e0e20e582c131aec119e667a5cf8360c3d271
Myze <myze2016@gmail.com>
2021-06-14 20:50:23 +0800

    Lastest code as of June 14 2021

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,126 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Models\User;
+    use Illuminate\Http\Response;
+    use App\Traits\ApiResponser;
+    use Illuminate\Http\Request;
+    use DB;
+
+    Class UserController extends Controller {
+
+        use ApiResponser;
+
+        private $request;
+
+        public function __construct(Request $request){
+            $this->request = $request;
+        }
+        
+        public function getUsers(){
+            $users = DB::connection('mysql')->select('Select * from tbl_user');
+
+            return $this->successResponse($users);
+        }
+
+        /**
+         * Return the list of users
+         * @return Illuminate\Http\Response
+         */
+
+        public function index(){
+            $users = User::all();
+            return $this->successResponse($users);
+        }
+
+        /*public function getUsers(){
+            $users = User::all();
+            return response()->json($users, 200);
+        }*/
+
+        public function add(Request $request){
+            $rules = [
+                'username' => 'required|max:20',
+                'password' => 'required|max:20',
+                'gender' => 'required|in:Male,Female',
+            ];
+
+            $this->validate($request, $rules);
+            $user = User::create($request->all());
+            return $this->successResponse($user, Response::HTTP_CREATED);
+        }
+
+        /**
+        * Obtains and show one user
+        * @return Illuminate\Http\Response
+        */
+        
+        public function show($id){
+
+            $user = User::findOrFail($id);
+            return $this->successResponse($user);
+
+            /*$user = User::where('userid', $id)->first();
+            if ($user){
+                return $this->successResponse($user);
+            }
+            {
+            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+            }*/
+        }
+
+        /**
+        * Update an existing author
+        * @return Illuminate\Http\Response
+        */
+        public function update(Request $request, $id){
+            $rules = [
+                'username' => 'max:20',
+                'password' => 'max:20',
+                'gender' => 'in:Male,Female',
+            ];
+
+            $this->validate($request, $rules);
+
+            //$user = User::findOrFail($id);
+            $user = User::where('userid', $id)->first();
+
+            if ($user){
+            $user->fill($request->all());
+            // if no changes happen
+            if ($user->isClean()) {
+                return $this->errorResponse('At least one value must change', 
+                Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $user->save();
+            return $this->successResponse($user);
+            }
+            /*{
+            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+            }*/
+        }
+
+        /**
+         * Remove an existing user
+         * @return Illuminate\Http\Response
+         */
+
+         public function delete($id){
+             $user = User::findOrFail($id);
+             $user->delete();
+             return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+             
+             //$user = User::where('userid', $id)->first();
+             /*if ($user){
+                 $user->delete();
+                 return $this->successResponse($user);
+             }
+             {
+                return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+             }*/
+         }
+
+}
+
+?>
---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+    protected $table = 'tbluser';
+    protected $fillable = [
+        'username', 'password', 'gender', 'userid'
+    ];
+
+    public $timestamp = false;
+    protected $primaryKey = 'userid';
+
+    protected $hidden = ['password'];
+}
+
+?>
